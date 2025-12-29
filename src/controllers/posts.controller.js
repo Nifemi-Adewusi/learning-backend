5108973a929846c1100771376480bc39eb517486
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-12-29 02:13:18 +0100

    Update and refactor the post response

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -4,16 +4,32 @@ import { Post } from "../models/post.model.js";
 const createPost = async (req, res) => {
   try {
     const { name, description, age } = req.body;
+
     if (!name || !description || !age) {
       return res
         .status(400)
-        .json({ message: "Please Provide Name, Description Or Age" });
+        .json({ message: "Please Provide Name, Description And Age" });
     }
 
-    const post = await Post.create(name, description, age);
-    res.status(201).json({ message: "Post Created Sucessfully" });
+    const post = await Post.create({
+      name,
+      description,
+      age,
+    });
+
+    res.status(201).json({
+      message: "Post Created Successfully",
+      post: {
+        id: post._id,
+        description: post.description,
+        name: post.name,
+        age: post.age,
+      },
+    });
   } catch (err) {
+    console.error(err);
     res.status(500).json({ message: "Failed To Create Post" });
   }
 };
+
 export { createPost };
