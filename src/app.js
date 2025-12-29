12b48b440f47cd7a706000a65aae2856cb83925e
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-12-29 01:45:34 +0100

    Update the app to use the newly created post route

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ import express from "express";
 
 import userRouter from "./routes/user.routes.js";
 
+import postRouter from "./routes/post.routes.js";
+
 // Create an express app
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
+app.use("api/v1/posts", postRouter);
 
 // app.use((req, res, next) => {
 //   console.log(req.method, req.url);
