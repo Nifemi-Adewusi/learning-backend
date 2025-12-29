3b88a7c89566b3c43017dccd5bb2ff5ac3233ad4
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-12-29 01:08:05 +0100

    Add the logout route to the user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/users.controllers.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/users.controllers.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/logout").post(logoutUser);
 export default router;
