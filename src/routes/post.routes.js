74b347c77f36e0b91354c15912753f4f67c39df1
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-12-29 03:12:24 +0100

    Update the post route to include the update route

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { createPost, getPosts } from "../controllers/posts.controller.js";
+import {
+  createPost,
+  getPosts,
+  updatePosts,
+} from "../controllers/posts.controller.js";
 
 const router = Router();
 
 router.route("/create").post(createPost);
 router.route("/get-posts").get(getPosts);
+router.route("/update/:id").patch(updatePosts);
 
 export default router;
